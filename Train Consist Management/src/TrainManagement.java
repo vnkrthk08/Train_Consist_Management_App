193e35628da94160b860c43d655e7f080003666d
vnkrthk08 <vinaykartheek.bathala@gmail.com>
2026-04-06 09:04:05 +0530

    UC5: Preserve insertion order and uniqueness using LinkedHashSet

--- Train Consist Management/src/TrainManagement.java
+++ Train Consist Management/src/TrainManagement.java
@@ -2,18 +2,13 @@ import java.util.*;
 
 public class TrainManagement {
     public static void main(String[] args) {
-        LinkedList<String> train = new LinkedList<>();
+        Set<String> train = new LinkedHashSet<>();
 
         train.add("Engine");
         train.add("Sleeper");
-        train.add("AC");
         train.add("Cargo");
         train.add("Guard");
-
-        train.add(2, "Pantry");
-
-        train.removeFirst();
-        train.removeLast();
+        train.add("Sleeper");
 
         System.out.println(train);
     }
