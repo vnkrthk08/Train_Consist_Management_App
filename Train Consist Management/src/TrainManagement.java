0668c90497d89308e33c9ec10ddd456e21e75ec6
vnkrthk08 <vinaykartheek.bathala@gmail.com>
2026-04-06 09:01:05 +0530

    UC1: Initialize train and display empty consist summary

---
+++ Train Consist Management/src/TrainManagement.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class TrainManagement {
+    public static void main(String[] args) {
+        System.out.println("=== Train Consist Management App ===");
+        List<String> train = new ArrayList<>();
+        System.out.println("Initial bogie count: " + train.size());
+    }
+}
